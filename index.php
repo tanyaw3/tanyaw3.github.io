e556b9f5b55602ff35bfba5ac6b5ef6d0ab334d8
Tanya Wang <tanyaw7@uw.edu>
2019-07-28 16:31:52 -0700

    Update nav, add clock

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, maximum-scale=1.0" />
+  <link rel="shortcut icon" type="image/x-icon" href="images/logo.png" />
+  <title>Tanya Wang</title>
+
+  <link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Oswald:200,400" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- Header -->
+  <div id="container">
+    <div class="triangle"></div>
+
+    <!-- Nav Start -->
+    <?php include "nav.php"; ?>
+
+    <!-- Nav End -->
+    <div class="header">
+      <h1>tanya wang</h1>
+      <h2>a work in progress</h2>
+      <p></p>
+    </div>
+  </div>
+
+  <!-- Approach -->
+  <div class="approachheader">
+    <h3> APPROACH </h3>
+  </div>
+
+  <div class="about">
+    <div class="assignments">
+      <div class="assignment">
+        <div class="noimage">
+          <h4>Discover</h4>
+          <ul>
+            <li>Focus on the person and the community</li>
+            <li>Systems Designs thinking- what should we build and why?</li>
+            <li>Collaborating across teams</li>
+            <li>Building digital experiences- fb.gg/home, facebook.com/creators, and more to come</li>
+            <li>Helping people grow their careers </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="assignment">
+        <div class="noimage">
+          <h4>Discover</h4>
+          <ul>
+            <li>Focus on the person and the community</li>
+            <li>Systems Designs thinking- what should we build and why?</li>
+            <li>Collaborating across teams</li>
+            <li>Building digital experiences- fb.gg/home, facebook.com/creators, and more to come</li>
+            <li>Helping people grow their careers </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="assignment">
+        <div class="noimage">
+          <h4>Discover</h4>
+          <ul>
+            <li>Focus on the person and the community</li>
+            <li>Systems Designs thinking- what should we build and why?</li>
+            <li>Collaborating across teams</li>
+            <li>Building digital experiences- fb.gg/home, facebook.com/creators, and more to come</li>
+            <li>Helping people grow their careers </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="assignments">
+      <div class="assignment">
+        <div class="noimage">
+          <h4>Discover</h4>
+          <ul>
+            <li>Focus on the person and the community</li>
+            <li>Systems Designs thinking- what should we build and why?</li>
+            <li>Collaborating across teams</li>
+            <li>Building digital experiences- fb.gg/home, facebook.com/creators, and more to come</li>
+            <li>Helping people grow their careers </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="assignment">
+        <div class="noimage">
+          <h4>Discover</h4>
+          <ul>
+            <li>Focus on the person and the community</li>
+            <li>Systems Designs thinking- what should we build and why?</li>
+            <li>Collaborating across teams</li>
+            <li>Building digital experiences- fb.gg/home, facebook.com/creators, and more to come</li>
+            <li>Helping people grow their careers </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="assignment">
+        <div class="noimage">
+          <h4>Discover</h4>
+          <ul>
+            <li>Focus on the person and the community</li>
+            <li>Systems Designs thinking- what should we build and why?</li>
+            <li>Collaborating across teams</li>
+            <li>Building digital experiences- fb.gg/home, facebook.com/creators, and more to come</li>
+            <li>Helping people grow their careers </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Projects -->
+  <div class="assignheader">
+    <h3>
+      <a href="#prototypes">
+        PROTOTYPES </br>
+        &nbsp;v
+      </a>
+    </h3>
+  </div>
+
+  <!-- Projects -->
+  <a name="prototypes"></a>
+
+
+  <div class="assignments">
+    <div class="assignment">
+      <a href="assignment9.html">
+        <h3>Weather Clock</h3>
+        <img src="images/a9/final_clock2.jpg" width=200 />
+      </a>
+      <p>
+        Arduino with ESP8266
+      </p>
+    </div>
+
+    <div class="assignment">
+      <a href="assignment1.html">
+        <h3>Geometric Lamp Shade</h3>
+        <img src="images/a1/finallamp.jpg" width=250 />
+      </a>
+      <p>
+        Made with Adobe Illustrator
+      </p>
+    </div>
+
+
+    <div class="assignment">
+      <a href="assignment2.html">
+        <h3>Papercut Story Box</h3>
+        <img src="images/a2/final-v3.jpg" width=250 />
+      </a>
+      <p>
+        Made with Grasshopper & Rhino
+      </p>
+    </div>
+
+    <div class="assignment">
+      <a href="assignment3.html">
+        <h3>I Love Lamp</h3>
+        <img src="images/a3/lamp-final.png" width=285 />
+      </a>
+      <p>
+        I know why lamps are expensive now.
+      </p>
+    </div>
+
+    <div class="assignment">
+      <a href="assignment4.html">
+        <h3>Rolling Desert</h3>
+        <img src="images/a4/finalv2.jpg" width=250 />
+      </a>
+      <p>
+        Made with Onshape
+      </p>
+    </div>
+
+    <div class="assignment">
+      <a href="assignment6-v2.html">
+        <h3>chocolate cones</h3>
+        <img src="images/a6/finalstack.jpg" width=250 />
+      </a>
+      <p>
+        Molding and Casting
+      </p>
+    </div>
+
+    <div class="assignment">
+      <a href="assignment7.html">
+        <h3>ferris wheel proposal</h3>
+        <img src="images/a7/sketch2.jpg" width=250 />
+      </a>
+      <p>
+        Plans for my ferris wheel
+      </p>
+    </div>
+
+    <div class="assignment">
+      <a href="assignment8.html">
+        <h3>ferris wheel for sweets</h3>
+        <img src="images/a8/final.jpg" width=200 />
+      </a>
+      <p>
+        A culmination of techniques
+      </p>
+    </div>
+
+
+
+  </div>
+
+</body>
+
+</html>
